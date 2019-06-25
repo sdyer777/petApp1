66a65d6e94711c30e810307acf2ba1ebe8b82c9f
sdyer777 <sdyer777@yahoo.com>
2019-06-25 19:08:51 -0400

    displaying list of pets

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+# uses api calls to get/post all data
+gem 'rest-client'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+  
+  # GET /
+  # Returns all pets
   def index
+    url = 'https://petapi-1.herokuapp.com/'
+    @response = RestClient.get(url)
+    @parsed = JSON.parse(@response)
+    @message = "Hello, how are you today?"
   end
+
+  # GET /widgets/1
+  # Returns pet with specified ID
+  def show
+    url = 'https://petapi-1.herokuapp.com/pets/'
+    @response = RestClient.get(url)
+  end
+
 end
