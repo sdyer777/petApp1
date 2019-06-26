6dc6873211e2f674e86303aad793428442da3b49
sdyer777 <sdyer777@yahoo.com>
2019-06-26 17:46:08 -0400

    got pet id query working on the details screen.  Committing to confirm it works up on heroku before moving on.

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -3,7 +3,9 @@ class DetailsController < ApplicationController
      # get pet info
     url = 'https://petapi-1.herokuapp.com/' + params[:id]
     @response = RestClient.get(url)
-    @pet = JSON.parse(@response)
+    @pets = JSON.parse(@response)
+    @pet = @pets[0]
+
     # GET /
   # Get the weather
     url = 'https://api.darksky.net/forecast/3be3bbf389ba38c8d1107cf491700c8e/42.2529,-71.0023'
