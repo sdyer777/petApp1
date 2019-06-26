3d8fbfa3442408587bd2df1deaa9644f7ee1b074
sdyer777 <sdyer777@yahoo.com>
2019-06-26 19:10:49 -0400

    code cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,9 @@
 class HomeController < ApplicationController
   
   # GET /
-  # GET /pet/1
   # Returns all pets
   def index
     url = 'https://petapi-1.herokuapp.com/'
-    @response = RestClient.get(url)
-    @parsed = JSON.parse(@response)
-    @message = "Hello, how are you today?"
-  end
-
-  # GET /widgets/1
-  # Returns pet with specified ID
-  def show
-    url = 'https://petapi-1.herokuapp.com/pets/'
     @response = RestClient.get(url)
     @parsed = JSON.parse(@response)
   end
